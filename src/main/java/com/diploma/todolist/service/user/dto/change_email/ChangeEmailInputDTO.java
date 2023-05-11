8edaea39c61cdda50cee18f6e55cca85c04f1917
mykolamurmil <nikolay.murmil@gmail.com>
2023-05-11 19:45:07 +0300

    Add change email validation

--- src/main/java/com/diploma/todolist/service/user/dto/change_email/ChangeEmailInputDTO.java
+++ src/main/java/com/diploma/todolist/service/user/dto/change_email/ChangeEmailInputDTO.java
@@ -5,10 +5,13 @@ import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 
+import javax.validation.constraints.Email;
+
 @Data
 @RequiredArgsConstructor
 @Getter
 @Setter
 public class ChangeEmailInputDTO {
+    @Email
     private String newEmail;
 }
